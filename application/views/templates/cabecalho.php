21fe725291f9fd130b67e3b257fe9f7ad878f5ce
magnoo <magno.oliveira@infnet.edu.br>
2016-03-16 14:44:42 -0300

    add os arquivos do bootstrap

--- application/views/templates/cabecalho.php
+++ application/views/templates/cabecalho.php
@@ -1,21 +1,19 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="utf-8" />
-
-    <title>Sistema de resultados do modelo de bloco - Instituto Infnet</title>
-
+    <title>Sistema de Resultados do Modelo de Bloco - Instituto Infnet</title>
     <link rel="icon" href="<?php echo site_url('../assets/img/favicon.ico') ?>" type="image/ico"/>
+    <link rel="stylesheet" href="<?php echo site_url('../assets/css/bootstrap/bootstrap.css')?>" type="text/css"/>
 
-<?php
-foreach($css_files as $file): ?>
-    <link rel="stylesheet" href="<?php echo $file; ?>" type="text/css"/>
-<?php endforeach; ?>
+    <?php foreach($css_files as $file): ?>
+        <link rel="stylesheet" href="<?php echo $file; ?>" type="text/css"/>
+    <?php endforeach; ?>
 
     <link rel="stylesheet" href="<?php echo site_url('../assets/css/estilo.css')?>" type="text/css"/>
 
-
-<?php foreach($js_files as $file): ?>
-    <script src="<?php echo $file; ?>"></script>
-<?php endforeach; ?>
+    <?php foreach($js_files as $file): ?>
+        <script src="<?php echo $file; ?>"></script>
+    <?php endforeach; ?>
 </head>
+<body>
--- application/views/templates/rodape.php
+++ application/views/templates/rodape.php
@@ -1,3 +1,6 @@
-                <p>&copy; 2016 <a href="http://infnet.edu.br" target="blank">Instituto Infnet</a></p>
-        </body>
+        <div class="footer">
+            <p>&copy; 2016 <a href="http://infnet.edu.br" target="blank">Instituto Infnet</a></p>
+        </div>
+        <script src="<?php echo site_url('../assets/js/bootstrap.min.js')?>"></script>
+    </body>
 </html>
