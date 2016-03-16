ae90caffb03b09e95393ec8dd31fafe617d675fd
magnoo <magno.oliveira@infnet.edu.br>
2016-03-16 18:43:52 -0300

    Preparado para receber as classes e scripts do bootstrap

--- application/views/templates/cabecalho.php
+++ application/views/templates/cabecalho.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
 <html lang="pt-br">
 <head>
     <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>Sistema de Resultados do Modelo de Bloco - Instituto Infnet</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="icon" href="<?php echo site_url('../assets/img/favicon.ico') ?>" type="image/ico"/>
     <link rel="stylesheet" href="<?php echo site_url('../assets/css/bootstrap/bootstrap.css')?>" type="text/css"/>
 
@@ -15,5 +23,6 @@
     <?php foreach($js_files as $file): ?>
         <script src="<?php echo $file; ?>"></script>
     <?php endforeach; ?>
+    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
 </head>
 <body>
--- application/views/templates/rodape.php
+++ application/views/templates/rodape.php
@@ -1,6 +1,9 @@
         <div class="footer">
             <p>&copy; 2016 <a href="http://infnet.edu.br" target="blank">Instituto Infnet</a></p>
         </div>
-        <script src="<?php echo site_url('../assets/js/bootstrap.min.js')?>"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<?php echo site_url("../assets/js/vendor/jquery-1.11.2.min.js")?>')</script>
+        <script src="<?php echo site_url('../assets/js/vendor/bootstrap.min.js')?>"></script>
+        <script src="js/main.js"></script>
     </body>
 </html>
