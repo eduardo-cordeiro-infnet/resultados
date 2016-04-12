7806e70116902a9d6cac69a44c85a2c8c0248d67
Eduardo Cordeiro <eduardo.cordeiro@infnet.edu.br>
2016-04-12 17:25:01 -0300

    Constantes de nomes de assessment final
    Incluir constante de sistema com os possíveis nomes de assessments
    finais

--- application/config/my_constants.php
+++ application/config/my_constants.php
@@ -1,2 +1,14 @@
 <?php
 defined('URL_BASE_LMS') OR define('URL_BASE_LMS', 'http://lms.infnet.edu.br/moodle');
+defined('SUBCOMPETENCIA_SIMBOLO_OBRIGATORIEDADE') OR define('SUBCOMPETENCIA_SIMBOLO_OBRIGATORIEDADE', '*');
+defined('NOMES_ASSESSMENT_FINAL') OR define('NOMES_ASSESSMENT_FINAL',
+	serialize(array(
+		'Assessment final',
+		'AT',
+		'Assessment',
+		'AP',
+		'Apresentação',
+		'Apresentação final'
+	))
+);
+defined('NOME_ASSESSMENT_FINAL') OR define('NOME_ASSESSMENT_FINAL', unserialize(NOMES_ASSESSMENT_FINAL)[0]);
